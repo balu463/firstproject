666773ca9786d87d1ca584220ce829088dae7154
Tech <diviseemabalu@gmail.com>
2016-10-28 22:22:56 -0700

    chnge server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ http.createServer(function(request,response){
 
 	response.writeHead(200,{"content-Type":"text/plain"});
 	response.end("hello hai hyde");
-}).listen(8090,function(){
-	console.log("server litening 8090");
+}).listen(8010,function(){
+	console.log("server litening 8010");
 });
 
